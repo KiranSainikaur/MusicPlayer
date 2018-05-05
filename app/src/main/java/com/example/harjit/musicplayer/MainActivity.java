6a53eaa0893e2694c11b21e972916f9eb6ad1ebb
KiranSainikaur <kaurkirarpreet3@gmail.com>
2018-05-05 10:13:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/harjit/musicplayer/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.harjit.musicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.terikhairmangdi);
+
+        Button playButton = findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this,"I am done",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        Button pauseButton = findViewById(R.id.pause_button);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
